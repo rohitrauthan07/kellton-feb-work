1e80d6b5d86cc6579d22d5e42cdc31d05a141e86
rohitrauthan07 <rohit07rsr@gmail.com>
2019-02-26 20:57:34 +0530

    Add files via upload

---
+++ 26feb19/dynamic.js
@@ -0,0 +1,28 @@
+<script>
+function addRow(tableID) {
+
+    var table = document.getElementById(tableID);
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    var colCount = table.rows[0].cells.length;
+
+    for (var i = 0; i < colCount; i++) {
+        var newcell = row.insertCell(i);
+        newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+        //alert(newcell.childNodes);
+        switch (newcell.childNodes[0].type) {
+            case "text":
+                newcell.childNodes[0].value = "";
+                break;
+            case "checkbox":
+                newcell.childNodes[0].checked = false;
+                break;
+            case "select-one":
+                newcell.childNodes[0].selectedIndex = 0;
+                break;
+        }
+    }
+}
+</script>
